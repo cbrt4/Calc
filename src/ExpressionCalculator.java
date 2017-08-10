f544351e674e84a812e974b6575868f6b1f5dbd5
alex <konstrukzer@gmail.com>
2017-08-11 00:45:24 +0300

    added javadoc, cotangent and factorial

--- src/ExpressionCalculator.java
+++ src/ExpressionCalculator.java
@@ -4,10 +4,10 @@ import java.util.stream.Stream;
 
 public class ExpressionCalculator {
 
-    private final List<String> functions = Stream.of("sin", "cos", "tan", "!")
+    private final List<String> functions = Stream.of("sin", "cos", "tan", "cot", "!")
             .collect(Collectors.toList());
 
-    private final List<String> operators = Stream.of("-", "+", "/", "*", "%", "^")
+    private final List<String> operators = Stream.of("-", "+", "/", "*", "^")
             .collect(Collectors.toList());
 
     private final List<String> brackets = Stream.of("(", ")")
@@ -18,35 +18,122 @@ public class ExpressionCalculator {
     private Stack<String> stack;
     private Stack<String> out;
 
-    private double result;
-
     public ExpressionCalculator() {
         constants = new HashMap<>();
         constants.put("pi", Math.PI);
         constants.put("e", Math.E);
     }
 
+    private List<String> getTokens(String expression) {
+
+        expression = expression.toLowerCase();
+
+        if (expression.charAt(0) == '-' || expression.charAt(0) == '+')
+            expression = "0" + expression;
+
+        expression = expression
+                .replace(",", ".")
+                .replaceAll("['`]|[ ]+", "")
+                .replace("(-", "(0-")
+                .replace("(+", "(0+");
+
+        for (String function : functions) {
+            expression = expression.replace(function, " " + function + " ");
+        }
+
+        for (String operator : operators) {
+            expression = expression.replace(operator, " " + operator + " ");
+        }
+
+        for (String bracket : brackets) {
+            expression = expression.replace(bracket, " " + bracket + " ");
+        }
+        return Arrays.stream(expression.split("[ ]+")).collect(Collectors.toList());
+    }
+
+    /**
+     * Алгоритм toRPN:
+     *
+     * Пока есть ещё символы для чтения:
+     *
+     * Читаем очередной символ.
+     *
+     * Если символ является числом, добавляем его к выходной строке.
+     *
+     * Если символ является символом функции, помещаем его в стек.
+     *
+     * Если символ является открывающей скобкой, помещаем его в стек.
+     *
+     * Если символ является закрывающей скобкой:
+     * До тех пор, пока верхним элементом стека не станет открывающая скобка,
+     * выталкиваем элементы из стека в выходную строку. При этом открывающая
+     * скобка удаляется из стека, но в выходную строку не добавляется. Если
+     * стек закончился раньше, чем мы встретили открывающую скобку, это означает,
+     * что в выражении либо неверно поставлен разделитель, либо не согласованы скобки.
+     *
+     * Если символ является оператором о1, тогда:
+     * 1) пока…
+     * … (если оператор o1 право-ассоциированный) приоритет o1 меньше приоритета
+     * оператора, находящегося на вершине стека…
+     * … (если оператор o1 ассоциированный, либо лево-ассоциированный) приоритет
+     * o1 меньше либо равен приоритету оператора, находящегося на вершине стека…
+     * … выталкиваем верхний элемент стека в выходную строку;
+     * 2) помещаем оператор o1 в стек.
+     *
+     * Когда входная строка закончилась, выталкиваем все символы из стека в
+     * выходную строку. В стеке должны были остаться только символы операторов;
+     * если это не так, значит в выражении не согласованы скобки.
+     * */
+
+    private void toRPN(String expression) {
+
+        for (String token : getTokens(expression)) {
+            if (isConstant(token)) token = String.valueOf(constants.get(token));
+            if (isNumber(token)) out.push(token);
+            if (isFunction(token)) stack.push(token);
+            if (isOpenBracket(token)) stack.push(token);
+            if (isCloseBracket(token)) {
+                while (!stack.empty() && !isOpenBracket(stack.peek())) {
+                    out.push(stack.pop());
+                }
+                stack.pop();
+            }
+            if (isOperator(token)) {
+                while (!stack.empty() && operationPriority(token) <= operationPriority(stack.peek())) {
+                    out.push(stack.pop());
+                }
+                stack.push(token);
+            }
+        }
+        while (!stack.empty()) out.push(stack.pop());
+    }
+
+    /**
+     * Автоматизация вычисления выражений в обратной польской нотации
+     * основана на использовании стека.
+     * Алгоритм вычисления для стековой машины элементарен:
+     * 1.Обработка входного символа
+     * * Если на вход подан операнд, он помещается на вершину стека.
+     * * Если на вход подан знак операции, то соответствующая операция
+     * выполняется над требуемым количеством значений, извлечённых из
+     * стека, взятых в порядке добавления. Результат выполненной операции
+     * кладётся на вершину стека.
+     * 2.Если входной набор символов обработан не полностью, перейти к шагу 1.
+     * 3.После полной обработки входного набора символов результат вычисления
+     * выражения лежит на вершине стека.
+     */
+
     public double calculate(String expression) {
         stack = new Stack<>();
         out = new Stack<>();
         toRPN(expression);
-
-        //  !!! >>> CALCULATING MAGIC HERE: <<< !!! \\\
 
         for (String token : out) {
             if (isNumber(token)) stack.push(token);
             if (isOperator(token)) calculateAction(token);
             if (isFunction(token)) calculateFunction(token);
         }
-
-        result = Double.parseDouble(stack.peek());
-
-        System.out.println("\nExpression: " + expression);
-        System.out.println("\nOut:        " + out);
-        System.out.println("\nStack:      " + stack);
-        System.out.println("\nResult:     " + result);
-
-        return result;
+        return Double.parseDouble(stack.pop());
     }
 
     private void calculateAction(String token) {
@@ -86,60 +173,19 @@ public class ExpressionCalculator {
             case "tan":
                 stack.push(String.valueOf(Math.tan(arg)));
                 break;
+            case "cot":
+                stack.push(String.valueOf(1/Math.tan(arg)));
+                break;
+            case "!":
+                stack.push(String.valueOf(factorial(arg)));
+                break;
         }
     }
 
-    private List<String> getTokens(String expression) {
-
-        expression = expression.toLowerCase();
-
-        if (expression.charAt(0) == '-' || expression.charAt(0) == '+')
-            expression = "0" + expression;
-
-        expression = expression
-                .replace(",", ".")
-                .replaceAll("['`]|[ ]+", "")
-                .replace("(-", "(0-")
-                .replace("(+", "(0+");
-
-        for (String function : functions) {
-            expression = expression.replace(function, " " + function + " ");
-        }
-
-        for (String operator : operators) {
-            expression = expression.replace(operator, " " + operator + " ");
-        }
-
-        for (String bracket : brackets) {
-            expression = expression.replace(bracket, " " + bracket + " ");
-        }
-
-        return Arrays.stream(expression.split("[ ]+")).collect(Collectors.toList());
-    }
-
-    private void toRPN(String expression) {
-
-        //  !!! >>> SORTING MAGIC HERE: <<< !!! \\\
-
-        for (String token : getTokens(expression)) {
-            if (isConstant(token)) token = String.valueOf(constants.get(token));
-            if (isNumber(token)) out.push(token);
-            if (isFunction(token)) stack.push(token);
-            if (isOpenBracket(token)) stack.push(token);
-            if (isCloseBracket(token)) {
-                while (!stack.empty() && !isOpenBracket(stack.peek())) {
-                    out.push(stack.pop());
-                }
-                stack.pop();
-            }
-            if (isOperator(token)) {
-                while (!stack.empty() && operationPriority(token) <= operationPriority(stack.peek())) {
-                    out.push(stack.pop());
-                }
-                stack.push(token);
-            }
-        }
-        while (!stack.empty()) out.push(stack.pop());
+    private double factorial(double arg) {
+        double result = 1;
+        for (int i = 1; i <= arg; i++) result *= i;
+        return result;
     }
 
     private boolean isConstant(String token) {
@@ -171,10 +217,10 @@ public class ExpressionCalculator {
         return operators.contains(token);
     }
 
-    private int operationPriority(String token) {
-        int priority = -1;
+    private byte operationPriority(String token) {
+        byte priority = -1;
         if (token.equals("+") || token.equals("-")) priority = 0;
-        if (token.equals("*") || token.equals("/") || token.equals("%")) priority = 1;
+        if (token.equals("*") || token.equals("/")) priority = 1;
         if (isFunction(token) || token.equals("^")) priority = 2;
         return priority;
     }
